561421d58757f0b3bfb1425fa9e8f019cd3c0d8b
Wilko Dekkers <wilko.dekkers@gmail.com>
2021-05-02 17:18:21 +0200

    Add vertex and fragment shader

--- Project1/src/Application.cpp
+++ Project1/src/Application.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <string>
 #include <GL/glew.h>
 #include <GL/glut.h>
 
@@ -10,6 +12,47 @@ float positions[6] = {
 	 0.0f,  0.5f,
 	 0.5f, -0.5f
 };
+
+static unsigned int compile_shader(const GLuint type, const std::string& source)
+{
+	const GLuint id = glCreateShader(type);
+	const char* src = source.c_str();
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if (result == GL_FALSE)
+	{
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		const auto message = static_cast<char*>(_malloca(length * sizeof(char)));
+		glGetShaderInfoLog(id, length, &length, message);
+		std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!" << std::endl;
+		std::cout << message << std::endl;
+		glDeleteShader(id);
+		return 0;
+	}
+
+	return id;
+}
+
+static GLuint create_shader(const std::string& vertex_shader, const std::string& fragment_shader)
+{
+	const GLuint program = glCreateProgram();
+	const GLuint vs = compile_shader(GL_VERTEX_SHADER, vertex_shader);
+	const GLuint fs = compile_shader(GL_FRAGMENT_SHADER, fragment_shader);
+
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program;
+}
 
 void render()
 {
@@ -47,7 +90,33 @@ int main(const int argc, char** argv)
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, nullptr);
 
+	const std::string vertex_shader = 
+		"#version 330 core\n"
+		"\n"
+		"layout(location = 0) in vec4 position;"
+		"\n"
+		"void main()\n"
+		"{\n"
+		"	gl_Position = position;\n"
+		"}\n";
+	const std::string fragment_shader =
+		"#version 330 core\n"
+		"\n"
+		"layout(location = 0) out vec4 color;"
+		"\n"
+		"void main()\n"
+		"{\n"
+		"	color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+		"}\n";
+	const GLuint shader = create_shader(vertex_shader, fragment_shader);
+	glUseProgram(shader);
+	
+	const HWND h_wnd = GetConsoleWindow();
+	ShowWindow(h_wnd, SW_SHOW);
+
 	glutMainLoop();
+
+	glDeleteProgram(shader);
 
 	return 0;
 }
